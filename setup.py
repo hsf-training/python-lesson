3be7c389e3dcdab2a6b566450885ec7a34a921f5
Chris Burr <christopher.burr@cern.ch>
2019-10-21 12:19:20 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='python_lesson',
-    version='0.0.1',
+    version='0.0.2',
     description='Helper Functions for HSF Advanced Python Lesson',
     long_description=long_description,
     long_description_content_type='text/markdown',
